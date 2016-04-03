6f8e78a001cd9c269873d74a42941550d21214a0
Ilya <marhoily@hotmail.com>
2016-04-03 06:55:47 +0300

    cleanup

--- Oiraga/Protocol/BinaryReaderExtensions.cs
+++ Oiraga/Protocol/BinaryReaderExtensions.cs
@@ -3,26 +3,25 @@ using System.Text;
 
 namespace Oiraga
 {
-    
-    public static class Ext
+    public static class BinaryReaderExtensions
     {
-        public static string ReadAsciiString(this BinaryReader _binaryReader)
+        public static string ReadAsciiString(this BinaryReader reader)
         {
             var sb = new StringBuilder();
             while (true)
             {
-                var ch = _binaryReader.ReadByte();
+                var ch = reader.ReadByte();
                 if (ch == 0) break;
                 sb.Append((char)ch);
             }
             return sb.ToString();
         }
-        public static string ReadUnicodeString(this BinaryReader _binaryReader)
+        public static string ReadUnicodeString(this BinaryReader reader)
         {
             var sb = new StringBuilder();
             while (true)
             {
-                var ch = _binaryReader.ReadUInt16();
+                var ch = reader.ReadUInt16();
                 if (ch == 0) break;
                 sb.Append((char)ch);
             }
