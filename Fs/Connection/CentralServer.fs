a8d537cb87901f00dd61e49b4329d858340ef727
Ilya <marhoily@hotmail.com>
2016-04-05 23:54:58 +0300

    port CommandsSink

--- Fs/Connection/CentralServer.fs
+++ Fs/Connection/CentralServer.fs
@@ -65,3 +65,24 @@ let Connect (server, key : string) recorder log =
     webSocket.OnClose.Add onClose
     webSocket
 
+type Command = 
+    | Spawn of string
+    | Spectate
+    | Split
+    | Eject
+    | MoveTo of double*double
+
+let CommandsSink (webSocket: WebSocket) =
+    function
+    | Spawn(name) ->
+        webSocket.Send [| 
+            yield 0uy; 
+            yield! (Encoding.Unicode.GetBytes name) |]
+    | Spectate -> webSocket.Send [| 1uy |]
+    | Split -> webSocket.Send [| 17uy |]
+    | Eject -> webSocket.Send [| 21uy |]
+    | MoveTo(x, y) ->
+        webSocket.Send [| 
+            yield 16uy; 
+            yield! (BitConverter.GetBytes(int(x)));
+            yield! (BitConverter.GetBytes(int(y)))|]
