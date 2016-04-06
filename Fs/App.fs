b2491343f7460cf420a03b0f5c7e26e5b91312b2
Ilya <marhoily@hotmail.com>
2016-04-06 23:30:08 +0300

    refactor

--- Fs/App.fs
+++ Fs/App.fs
@@ -4,10 +4,24 @@ open System
 open System.Windows
 open System.Windows.Controls
 open System.Windows.Markup
+open System.Windows.Input
 
 type MainWindow = FsXaml.XAML<"MainWindow.xaml", true>
+type GameControl = FsXaml.XAML<"Ui/GameControl.xaml", true>
 let window = new MainWindow()
+window.Root.KeyDown.Add(fun e -> 
+    if e.Key = Key.F11 then
+        if window.Root.WindowStyle <> WindowStyle.None then
+            window.Root.WindowStyle <- WindowStyle.None
+            window.Root.WindowState <- WindowState.Maximized
+        else
+            window.Root.WindowState <- WindowState.Normal
+            window.Root.WindowStyle <- WindowStyle.SingleBorderWindow)
 
-[<STAThread>]
-[<EntryPoint>]
-let main(_) = (new Application()).Run(window.Root)
+//let gameControl = new GameControl()
+//window.GameControlPlace.Content <- gameControl.Root
+
+let app = new Application()
+
+[<STAThread; EntryPoint>]
+let main(_) = app.Run(window.Root)
